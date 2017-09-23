e4286cbf45fd7c587cb4391610ff90d995707fd7
NyxCode <moritz.bischof1@gmail.com>
2017-09-24 00:17:25 +0200

    annotate modified modules

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :string
+#  user_id     :integer
+#  category    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
