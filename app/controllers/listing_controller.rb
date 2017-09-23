c8941751e49bf1b0b161e6df2ab14e49e8a13155
NyxCode <moritz.bischof1@gmail.com>
2017-09-23 19:05:31 +0200

    implement GET /listings/, DELETE /listings/:id and PUT /listings/:id

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,19 +1,47 @@
 class ListingController < ApplicationController
-  def new_listing;
+
+  # TODO: remove before publication
+  def index_listings
+    render json: Listing.all.to_json
+  end
+
+  # TODO
+  def new_listing
   end
 
   def get_listing
-    listing = Listing.find_by_id(params[:id])
+    id = params[:id]
+    listing = Listing.find_by_id(id)
     if listing
       render json: listing.to_json
     else
-      render status: 404
+      render status: 204
     end
   end
 
-  def edit_listing;
+  # TODO: authorization
+  def edit_listing
+    id = params[:id]
+    listing = Listing.find_by_id(id)
+
+    title = params[:title]
+    description = params[:description]
+    category = params[:category]
+
+    render status: 204 and return unless listing
+
+    listing.title = title if title
+    listing.description = description if description
+    listing.category = category if category
+    listing.save
+
+    render json: listing.to_json
   end
 
-  def delete_listing;
+  # TODO: authorization
+  def delete_listing
+    id = params[:id]
+    success = Listing.delete(id) == 1
+    render status: (success ? 200 : 404)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-	# fetch all records (just for ease of dev, we'll remove this later)
+  # TODO: remove before publication
 	get '/' => 'application#fetch_all'
   
 	scope '/listings' do
+    # TODO: remove before publication
+    get '/' => 'listing#index_listings'
 		get '/:id' => 'listing#get_listing'
 		post '/new' => 'listing#new_listing'
 		put '/:id' => 'listing#edit_listing'
