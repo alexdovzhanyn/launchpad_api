45616e952d506c7d792a6538497d2c51d8783457
NyxCode <moritz.bischof1@gmail.com>
2017-09-23 18:13:56 +0200

    append schema information to models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :string
+#  user_id     :integer
+#  category    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Listing < ApplicationRecord
 end
