60dd051460345194445d867f13620f863a1313c2
NyxCode <moritz.bischof1@gmail.com>
2017-09-23 18:11:44 +0200

    implement READ operation for listings

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,15 +1,19 @@
 class ListingController < ApplicationController
+  def new_listing;
+  end
 
-	def new_listing
-	end
+  def get_listing
+    listing = Listing.find_by_id(params[:id])
+    if listing
+      render json: listing.to_json
+    else
+      render status: 404
+    end
+  end
 
-	def get_listing
-	end
+  def edit_listing;
+  end
 
-	def edit_listing
-	end
-
-	def delete_listing
-	end
-
+  def delete_listing;
+  end
 end
