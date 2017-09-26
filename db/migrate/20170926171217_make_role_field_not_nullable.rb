226dc7085608d925221b3453a037e8129a81728c
NyxCode <moritz.bischof1@gmail.com>
2017-09-26 19:47:54 +0200

    remove debug message

--- db/migrate/20170926171217_make_role_field_not_nullable.rb
+++ db/migrate/20170926171217_make_role_field_not_nullable.rb
@@ -1,6 +1,5 @@
 class MakeRoleFieldNotNullable < ActiveRecord::Migration[5.1]
   def change
-    puts User.column_names
     change_column_null :users, :role, false
     change_column_default :users, :role, User.roles[:user]
   end
