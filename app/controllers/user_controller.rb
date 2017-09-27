01124f966f7525fdfcc3976898b04b09be77788a
Alex <dovzhanyn.alex@gmail.com>
2017-09-27 00:08:17 -0400

    adds route for fetching user info and updates readme

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,15 @@
 class UserController < ApplicationController
+	before_action :authenticate_user!
 
+	# This is useful for things like viewing another user's profile
+	def get
+		user = User.find_by_id(params[:id])
+
+		if user
+      render json: user.attributes.extract!('email', 'first_name', 'last_name', 'created_at').to_json
+    else
+      render status: 204
+    end
+	end
 
 end
