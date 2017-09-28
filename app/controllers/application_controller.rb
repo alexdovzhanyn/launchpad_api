95f78e46f35ea1af3b82a717b8a5e6aec30613a8
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2017-09-28 18:50:26 +0000

    implement and configure sentry (#16)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :set_raven_context
 
 	def fetch_all
 		return_value = {
 			users: User.all,
 			listings: Listing.all
 		}
+		
 		render json: return_value.to_json
 	end
+
+
+	private
+
+  def set_raven_context
+    Raven.user_context(id: current_user ? current_user.id : 'Not signed in')
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 
 end
