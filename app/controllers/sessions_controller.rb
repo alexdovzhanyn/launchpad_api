aa1af5fd51b74c3b44a8312acf1642412f0130b9
Alex <dovzhanyn.alex@gmail.com>
2017-09-28 00:38:28 -0400

    finalize

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < DeviseTokenAuth::SessionsController
+	def new
+		super
+	end
+
+	def create
+		super
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'users'
-
+  mount_devise_token_auth_for 'User', at: 'user', controllers: {
+  	sessions: 'sessions',
+  	passwords: 'passwords',
+		registrations: 'registrations',
+		omniauth_callbacks: 'omniauth_callbacks'  	
+  }
 
   # TODO: remove before publication
 	get '/' => 'application#fetch_all'
@@ -13,5 +17,11 @@ Rails.application.routes.draw do
 		put '/:id' => 'listing#edit'
 		delete '/:id' => 'listing#delete'
 	end
+
+	scope '/user' do
+		get '/info/:id' => 'user#get'
+	end
+
+	
 
 end
